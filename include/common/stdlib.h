0e2e943772136e6e99af52f056d409b11c8e77b9
Paweł Kisielewicz <pkisiel@MacBook-Pro-Pawe.local>
2020-07-02 13:16:17 +0200

    initial commit

---
+++ include/common/stdlib.h
@@ -0,0 +1,10 @@
+#ifndef STDLIB_H
+#define STDLIB_H
+
+void memcpy(void * dest, void * src, int bytes);
+
+void bzero(void * dest, int bytes);
+
+char * itoa(int i);
+
+#endif
---
+++ src/kernel/kernel.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <kernel/uart.h>
+#include <common/stdio.h>
+#include <common/stdlib.h>
+
+void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags)
+{
+    char buf[256];
+    // Declare as unused
+    (void) r0;
+    (void) r1;
+    (void) atags;
+
+    uart_init();
+    puts("Hello, kernel World!\n");
+    puts("NanoOS Ready 0.0.1\n");
+    while (1) {
+        gets(buf,256);
+        puts(buf);
+        putc('\n');
+    }
+}
